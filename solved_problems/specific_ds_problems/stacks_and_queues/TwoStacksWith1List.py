9acab5bb8e4c9093c117f0cc48943993135b2f6a
saksham-s1ngh <saksham249singh@gmail.com>
2025-09-26 19:41:38 -0400

    twostacks with list: added provided solution

--- solved_problems/specific_ds_problems/stacks_and_queues/TwoStacksWith1List.py
+++ solved_problems/specific_ds_problems/stacks_and_queues/TwoStacksWith1List.py
@@ -19,7 +19,7 @@ pop2(): Removes the top element from the second stack and returns it.
 
 Note: Perform all operations in-place without resizing the underlying list, maintaining a fixed size throughout.
 """
-class TwoStacks:
+class TwoStacks_myFA:
     # Initialize the two stacks here
     def __init__(self, size):
         self.tS = [None]*size
@@ -66,6 +66,51 @@ class TwoStacks:
         self.tS[self.s2_ptr + 1] = None # the element at popped index is reset to 0
         self.s2_ptr += 1
         return popped
+
+class TwoStacks: # PROVIDED SOLUTION CLASS
+    def __init__(self, n):  
+        self.size = n
+        self.arr = [0] * n  
+        self.top1 = -1
+        self.top2 = self.size
+
+    def push1(self, value):
+        # Increment top pointer and add element to stack 1 if space is available, else print stack overflow and exit
+        if self.top1 < self.top2 - 1:
+            self.top1 += 1
+            self.arr[self.top1] = value
+        else:
+            print("Stack Overflow ")
+            exit(1)
+
+    def push2(self, value):
+        # Decrement top pointer and add element to stack 2 if space is available, else print stack overflow and exit
+        if self.top1 < self.top2 - 1:
+            self.top2 -= 1
+            self.arr[self.top2] = value
+        else:
+            print("Stack Overflow ")
+            exit(1)
+
+    def pop1(self):
+        # Return and remove top element from stack 1 if not empty, else print stack underflow and exit
+        if self.top1 >= 0:
+            value = self.arr[self.top1]
+            self.top1 -= 1
+            return value
+        else:
+            print("Stack Underflow ")
+            exit(1)
+
+    def pop2(self):
+        # Return and remove top element from stack 2 if not empty, else print stack underflow and exit
+        if self.top2 < self.size:
+            value = self.arr[self.top2]
+            self.top2 += 1
+            return value
+        else:
+            print("Stack Underflow ")
+            exit()
 
 def main():
     calls = [
