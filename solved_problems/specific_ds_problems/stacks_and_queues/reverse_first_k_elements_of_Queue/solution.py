db3c7305a1c1f561c9324fa0d27d13a47de2d117
saksham-s1ngh <saksham249singh@gmail.com>
2025-09-26 20:11:46 -0400

    Opt: reverse first k elems of Queue using Stack my first attempt was modyfying the original list but returning a new created one.
    the correct solution just modified the argument list. My solution was on the right track but
    missed this.

--- solved_problems/specific_ds_problems/stacks_and_queues/reverse_first_k_elements_of_Queue/solution.py
+++ solved_problems/specific_ds_problems/stacks_and_queues/reverse_first_k_elements_of_Queue/solution.py
@@ -1,7 +1,8 @@
 from Queue import MyQueue
 from Stack import MyStack
 
-
+# MY FIRST ATTEMPT: NOT AT IN-PLACE MODIFYING ALGORITHM
+#   INSTEAD RETURNS A NEW QUEUE
 def reverse_k_elements_myFA(queue, k):
     
     # Replace this placeholder return statement with your code
@@ -29,6 +30,29 @@ def reverse_k_elements_myFA(queue, k):
     
     return k_reversed_q
 
+# 1. Push the first k elements in the queue in a stack
+# 2. Pop the stack elements and enqueue them at the end of queue
+# 3. Dequeue queue elements till k and append them at the end of the queue
+
+def revers_k_elements(queue, k):
+    # Handling invalid input
+    if queue.is_empty() is True or k > queue.size() or k < 0:
+        return None
+
+    stack = MyStack()
+    for i in range(k):
+        stack.push(queue.dequeue())
+
+    while stack.is_empty() is False:
+        queue.enqueue(stack.pop())
+        
+    size = queue.size()
+    for i in range(size - k):
+        queue.enqueue(queue.dequeue())
+
+    return queue
+
+
 if __name__ == "__main__":
     test_cases = [
         [1,2,3,4,5,6,7,8,9,10],
@@ -45,6 +69,6 @@ if __name__ == "__main__":
         k = k_values[i]
         print(i+1, "\tOriginal Queue:", queue.queue_list)
         print("\tk value:", k)
-        reversed_queue = reverse_k_elements_myFA(queue, k)
+        reversed_queue = revers_k_elements(queue, k)
         print("\tQueue after reversal:" , queue.queue_list)
         print("-"*100)
