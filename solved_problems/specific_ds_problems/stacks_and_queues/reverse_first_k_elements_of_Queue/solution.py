050b2a89b37a6c1a8bdec02b8a78d046b5e15800
saksham-s1ngh <saksham249singh@gmail.com>
2025-09-26 20:07:44 -0400

    MyFA: reverse first k elems of Queue using stack implemented my first attempt to attemp this question. created an additional directory for this solution, since updating the Queue file again would've been two much of a hassle.

---
+++ solved_problems/specific_ds_problems/stacks_and_queues/reverse_first_k_elements_of_Queue/solution.py
@@ -0,0 +1,50 @@
+from Queue import MyQueue
+from Stack import MyStack
+
+
+def reverse_k_elements_myFA(queue, k):
+    
+    # Replace this placeholder return statement with your code
+    input_q_size = queue.size()
+    if k < 1 or k > input_q_size: 
+        return None
+        
+    
+    k_reversed_q = MyQueue()
+    rev_stack = MyStack()
+    
+    # i = 0
+    # while i < k:
+    for _ in range(k):
+        rev_stack.push(queue.dequeue())
+        # i += 1
+        
+    while rev_stack.size():
+        k_reversed_q.enqueue(rev_stack.pop()) 
+    
+    # while i < input_q_size:
+    while queue.size():
+        k_reversed_q.enqueue(queue.dequeue())
+        # i += 1
+    
+    return k_reversed_q
+
+if __name__ == "__main__":
+    test_cases = [
+        [1,2,3,4,5,6,7,8,9,10],
+        [-2,1,-5,45,6,3,-9],
+        [1,2,5,0,7,4,23],
+        [7,3,5,6,8,12],
+        [5,67,43,23,12,56,78,98,6,21,9]
+    ]
+    k_values = [4, 10, -7, 5, 2]
+    for i in range(len(test_cases)):
+        queue = MyQueue()
+        for item in test_cases[i]:
+            queue.enqueue(item)
+        k = k_values[i]
+        print(i+1, "\tOriginal Queue:", queue.queue_list)
+        print("\tk value:", k)
+        reversed_queue = reverse_k_elements_myFA(queue, k)
+        print("\tQueue after reversal:" , queue.queue_list)
+        print("-"*100)
