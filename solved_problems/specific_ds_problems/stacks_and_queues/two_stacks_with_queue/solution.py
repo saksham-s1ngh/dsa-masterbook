3c67fb68ecf2379b6dcc474705f40160dfe13940
saksham-s1ngh <saksham249singh@gmail.com>
2025-10-26 14:29:26 -0400

    Appended my solution with additional comments The core implementation is same for this for both the brute force method and the optimised version. Only difference being whether we choose to utilise the main_stack only, or use the sub_stack for the majority of our implementation, the optimised version being the latter.

--- solved_problems/specific_ds_problems/stacks_and_queues/two_stacks_with_queue/solution.py
+++ solved_problems/specific_ds_problems/stacks_and_queues/two_stacks_with_queue/solution.py
@@ -17,13 +17,17 @@ class NewQueue:
 
     # Inserts the element in the queue
     def enqueue(self, value):
-        # Write your code here
+        # Just push all the inserted values into the main stack
         self.main_stack.push(value)
         return
 
     # Removes the element from the queue
     def dequeue(self):
-        # Write your code here
+        # we always pop from the sub_stack since the main_stack will order elements as a stack
+        #   and to get the queue ordering we first pop all main stack elements -> push them 
+        #   into the sub_stack and then pop.
+        # if the sub_stack is empty, we first push all the main_stack elements into the sub_stack
+        #   and then pop the element from the sub_stack
         if self.sub_stack.is_empty():
             while self.main_stack.size():
                 self.sub_stack.push(self.main_stack.pop())
@@ -31,6 +35,9 @@ class NewQueue:
         return self.sub_stack.pop()
     
     def peek(self):
+        # return the top element on the sub-stack if not empty
+        # if sub_stack is empty then we first push all elements from the main_stack
+        #   into the sub_stack.
         if self.sub_stack.is_empty():
             while not self.main_stack.is_empty():
                 self.sub_stack.push(self.main_stack.pop())
